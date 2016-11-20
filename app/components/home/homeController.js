cfdebe3021813ed881edda4877d303fcfeb309e6
Shaheen Ahmed <s.ahmed@aol.com>
2016-11-20 09:33:31 -0500

    adds navigation new categories on top

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,4 +1,5 @@
 angular.module('feeder')
     .controller('homeController', function ($scope) {
-
+    	// navigation newscategories
+    	$scope.newsCategories = ["Business", "Entertainment", "Gaming", "General", "Music", "Science & Nature", "Sport", "Technology"];
     })
