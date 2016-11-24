c9a04fecd4462e2304025bbeca3693342a3506ce
Shaheen Ahmed <s.ahmed@aol.com>
2016-11-24 16:06:57 -0500

    adds story actions and reactions

--- app/components/news-story/newsStoryController.js
+++ app/components/news-story/newsStoryController.js
@@ -1,4 +1,25 @@
 angular.module('feeder')
-    .controller("newsStoryController", function ($scope) {
+    .controller("newsStoryController", function ($scope, $mdDialog) {
+    	var originatorEv;
+    	$scope.options = [
+    		{
+    			
+    		}
+    	]
 
+	    this.openMenu = function($mdOpenMenu, ev) {
+	      originatorEv = ev;
+	      $mdOpenMenu(ev);
+	    };
+
+	    this.announceClick = function(index) {
+	      $mdDialog.show(
+	        $mdDialog.alert()
+	          .title('You clicked!')
+	          .textContent('You clicked the menu item at index ' + index)
+	          .ok('Nice')
+	          .targetEvent(originatorEv)
+	      );
+	      originatorEv = null;
+	    };
     })
