28f24c28fcfb066560671cbabb783914553bdb45
Shaheen Ahmed <s.ahmed@aol.com>
2016-11-26 21:20:47 -0500

    removes reaction and action buttons for redesign

--- app/components/news-story/newsStoryController.js
+++ app/components/news-story/newsStoryController.js
@@ -1,42 +1,6 @@
 angular.module('feeder')
     .controller("newsStoryController", function ($scope, $mdDialog) {
-
-    	// news-story reactions status
     	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    	// share options dialog box
-    	$scope.shareOptions = ["Facebook", "Twitter", "Google+", "LinkedIn"];
-    	var originatorEv;
-	    this.openMenu = function($mdOpenMenu, ev) {
-	      originatorEv = ev;
-	      $mdOpenMenu(ev);
-	    };
-	    this.announceClick = function(index) {
-	      $mdDialog.show(
-	        $mdDialog.alert()
-	          .title('Google+')
-	          .textContent('The news has been shared on Google+')
-	          .ok('Ok')
-	          .targetEvent(originatorEv)
-	      );
-	      originatorEv = null;
-	    };
 
     })
