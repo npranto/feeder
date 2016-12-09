6177806ac6ce7066f3092fca926e850c5272d457
Nazmuz Pranto <npranto@rosettastone.com>
2016-12-09 10:49:59 -0500

    puts final touches on populateNewsSources.js

--- assets/scripts/populateNewsSources.js
+++ assets/scripts/populateNewsSources.js
@@ -1,7 +1,23 @@
 'use strict'
 
+/*
+ SCRIPT
+  populateNewsSources.js
+
+ USAGE
+  By default, running populateNewsSources.js will create an "id" key with a random uuid value for each newsSource
+  objects inside news-sources.json.
+
+ COMMAND
+  - go into project directory on command line
+  - move to 'assets/scripts'
+  - run 'npm run-script pop-source-ids'
+*/
+
 const jsonfile = require('jsonfile');
 const uuidV4 = require('uuid/v4');
+var _ = require('lodash');
+
 
 const newsSourceJSON = "./../sources/news-sources.json";
 
@@ -15,12 +31,13 @@ function getAllNewsSources(){
 function addNewsSourcesIds(){
   jsonfile.readFile(newsSourceJSON, function(err, obj) {
     returnJSON = obj;
-    for(let i=0; i<returnJSON.newsSources.length; i++){
-      let currSource = returnJSON.newsSources[i];
-      currSource['id'] = uuidV4();
-    }
+    _.map(returnJSON.newsSources, function (currNewsSource) {
+      currNewsSource['id'] = uuidV4();
+    })
     jsonfile.writeFileSync(newsSourceJSON, returnJSON, {spaces: 2});
   })
 }
 
+
+///////////////////
 addNewsSourcesIds();
