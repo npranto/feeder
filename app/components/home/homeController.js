ad1750572e3f0b39ab3fbe54f159a986b22dbb67
Shaheen Ahmed <s.ahmed@aol.com>
2016-12-11 03:11:12 -0500

    Add “news-source-logos” section inside aside-left, which should display all the news-source logos, which are clickable; When a “news-source-logo” is clicked, display the TLP-blocks to be clicked; Make default TLP-blocks hidden onload

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -3,6 +3,12 @@ angular.module('feeder')
     	// navigation newscategories
     	$scope.newsCategories = ["Business", "Entertainment", "Gaming", "General", "Music", "Science & Nature", "Sport", "Technology"];
 
+    	$scope.showNewsSourcesGallery = false;
+
+    	$scope.alterTLPGalleryDisplay = function () {
+            $scope.showNewsSourcesGallery = !$scope.showNewsSourcesGallery;
+        }
+
     	function getAllNewsSources(){
             homeServices.getAllNewsSources()
             .then(function(response) {
