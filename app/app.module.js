08d5dab0bec3f8e09e1d54acfdc4759c93068fbe
Nazmuz Shakib Pranto <npranto@gmail.com>
2016-12-17 14:44:49 -0500

    second attempt to webpack

--- app/app.module.js
+++ app/app.module.js
@@ -1,9 +1,3 @@
-let angular =  require('angular');
-
-import './imports/importControllers.js'
-import './imports/importDirectives.js'
-import './imports/importServices.js'
-
 angular.module('feeder', ['ui.router', 'ngMaterial']);
 
 
