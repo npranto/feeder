bf8570774bcbe4f73518ce365a276338451b234e
Shaheen Ahmed <s.ahmed@aol.com>
2016-12-23 22:24:03 -0500

    cleans up source code to provide clarity in component classes

--- app/APIs/news-api/NewsAPI.js
+++ app/APIs/news-api/NewsAPI.js
@@ -1,19 +1,5 @@
-// angular.module('feeder')
-//     .constant('newsAPI', {
-//         "apiKey": "apiKey=API_KEY",
-//         "and": "&",
-//         "article": {
-//             "endpoint": "https://newsapi.org/v1/articles?",
-//             "source": "source=",
-//             "sortBy": {
-//                 "top": "sortBy=top",
-//                 "latest": "sortBy=latest",
-//                 "popular": "sortBy=popular",
-//             },
-//         }
-//     })
-
 export default class NewsAPI {
+
     constructor() {
         this.apiKey = "apiKey=API_KEY",
         this.and =  "&",
@@ -27,5 +13,6 @@ export default class NewsAPI {
             },
         }
     }
+
 }
 
--- app/components/home/HomeServices.js
+++ app/components/home/HomeServices.js
@@ -1,29 +1,3 @@
-// angular.module('feeder')
-//     .service('homeServices', function ($http, _, newsAPI) {
-//
-//         this.getAllNewsSources = function () {
-//             return $http.get('assets/sources/news-sources.json');
-//         }
-//
-//         this.getLatestNews = function (latestNewsRoute) {
-//             console.log(latestNewsRoute);
-//             return $http.get(latestNewsRoute);
-//         }
-//
-//         this.getNewsFromSource = function (newsSourceFormat) {
-//             let route = newsAPI.article.endpoint +
-//                 newsAPI.article.source +
-//                 newsSourceFormat +
-//                 newsAPI.and +
-//                 newsAPI.article.sortBy.top +
-//                 newsAPI.and +
-//                 newsAPI.apiKey;
-//             return $http.get(route);
-//         }
-//
-//     })
-
-
 export default class HomeServices {
 	constructor($http, NewsAPI) {
 		this.$http = $http;
